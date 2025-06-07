fb33e0f077ed46dd84a24e6c759c91545da8d12e
Vadim Firulyov <firulvv@mail.ru>
2025-06-08 01:02:03 +0400

    add breakpoints

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/shared/ui/button";
 
 export default function Home() {
   return (
-    <div className="max-mobile:hidden max-mobi grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <Button>Click</Button>
     </div>
   );
