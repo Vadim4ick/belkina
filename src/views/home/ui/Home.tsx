ede5c25a3baccf7d78aaca37e24dc24ef6fbb606
Vadim Firulyov <firulvv@mail.ru>
2025-06-18 23:42:45 +0400

    ✨ (Home.tsx): добавлен новый компонент AboutBanner и изображение о проекте Добавлен новый компонент AboutBanner, который предоставляет информацию о проекте и его преимуществах. Это улучшает пользовательский интерфейс, добавляя раздел, который помогает пользователям лучше понять цель проекта. Также добавлено изображение about.png для визуального оформления этого раздела.

--- src/views/home/ui/Home.tsx
+++ src/views/home/ui/Home.tsx
@@ -1,12 +1,14 @@
 import { TariffList } from "@/features/tariff-list";
 import { MainBanner } from "./home-banners/main-banner";
 import { TestsBanner } from "./home-banners/tests-banner";
+import { AboutBanner } from "./home-banners/about-banner";
 
 const Home = () => {
   return (
     <>
       <MainBanner />
       <TariffList />
+      <AboutBanner />
       <TestsBanner />
     </>
   );
