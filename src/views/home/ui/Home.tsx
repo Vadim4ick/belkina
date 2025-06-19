3214ed506134d8cf409f27227ff4ce3d6df43906
Vadim Firulyov <firulvv@mail.ru>
2025-06-19 18:15:18 +0400

    частозадаваемые вопросы

--- src/views/home/ui/Home.tsx
+++ src/views/home/ui/Home.tsx
@@ -2,6 +2,7 @@ import { TariffList } from "@/features/tariff-list";
 import { MainBanner } from "./home-banners/main-banner";
 import { TestsBanner } from "./home-banners/tests-banner";
 import { AboutBanner } from "./home-banners/about-banner";
+import { AskedQuestions } from "@/features/asked-questions";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <MainBanner />
       <TariffList />
       <AboutBanner />
+      <AskedQuestions />
       <TestsBanner />
     </>
   );
