38768b1004d702ba88c317c13aef5048150db7f3
Vadim <a-yva-2013@mail.ru>
2025-06-22 23:33:46 +0400

    add hooks

--- src/shared/hooks/useGqlClient.ts
+++ src/shared/hooks/useGqlClient.ts
@@ -1,7 +1,7 @@
 'use client'
 
 import { useSession } from 'next-auth/react'
-import { createGqlClient } from './client'
+import { createGqlClient } from '../graphql/client'
 
 export const useGqlClient = () => {
   const { data: session } = useSession()
