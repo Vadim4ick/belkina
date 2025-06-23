e22e4ea37d16b9b33c5fc057fd68f91157ac973b
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-06-23 16:27:18 +0300

    fix: настроен сайдбар личношгокабинета

--- src/widgets/app-sidebar/app-sidebar.tsx
+++ src/widgets/app-sidebar/app-sidebar.tsx
@@ -1,15 +1,10 @@
 import { PrivateMenuItems } from './_vm/privite-menu-items'
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarHeader,
-  SidebarMenu,
-} from '@/shared/ui/sidebar'
+import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader } from '@/shared/ui/sidebar'
 import { NavMain } from './_ui/nav-main'
-import { UserProfile } from '../app-header/ui/user-profile'
 import { Logo } from '@/shared/ui/logo'
 import { FC, SVGProps } from 'react'
+import { NavUser } from './_ui/nav-user'
+import { useSession } from 'next-auth/react'
 
 export interface IsideBarItems {
   title: string
@@ -18,19 +13,16 @@ export interface IsideBarItems {
 }
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const { data: session, status } = useSession()
+
   return (
     <Sidebar collapsible="offcanvas" {...props}>
-      <SidebarHeader>
-        <SidebarMenu>
-          <Logo />
-        </SidebarMenu>
-      </SidebarHeader>
+      <SidebarHeader></SidebarHeader>
       <SidebarContent>
+        <Logo />
         <NavMain items={PrivateMenuItems} />
       </SidebarContent>
-      <SidebarFooter>
-        <UserProfile className="flex-row-reverse" />
-      </SidebarFooter>
+      <SidebarFooter>{session?.user && <NavUser user={session.user} />}</SidebarFooter>
     </Sidebar>
   )
 }
