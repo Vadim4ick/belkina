937910c0aa4bddef034f3cb798ca4f74e4e41e92
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-06-25 22:00:32 +0300

    fix: изменение цвета беджа теста в зависимости от статуса

--- src/views/recomendations/ui/test-item.tsx
+++ src/views/recomendations/ui/test-item.tsx
@@ -5,7 +5,7 @@ import { TestsListItem } from '@/widgets/tests-history/ui/tests-history'
 const TestItem = ({ test }: { test: TestsListItem }) => {
   return (
     <>
-      <div className="grid gap-3 rounded-[6px] bg-white px-2.5 py-3 lg:grid-cols-8 lg:grid-rows-1">
+      <div className="grid items-center gap-3 rounded-[6px] bg-white px-2.5 py-3 lg:grid-cols-8 lg:grid-rows-1">
         <div className="lg:col-span-4">
           <Typography tag="p" variant="poppins-reg-14" className="text-[#6B7280]">
             {test.name}
@@ -17,7 +17,7 @@ const TestItem = ({ test }: { test: TestsListItem }) => {
           </Typography>
         </div>
         <div className="justify-self-end lg:col-span-2 lg:col-start-7">
-          <Badge variant="success" className="px-2.5">
+          <Badge variant={test.status == 'Пройдено' ? 'success' : 'destructive'} className="px-2.5">
             <Typography tag="p" variant="poppins-reg-14">
               {test.status}
             </Typography>
