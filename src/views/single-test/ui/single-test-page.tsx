3d30f2467640325c11eba8be29f0fb2217c0b94f
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-06-27 11:03:25 +0300

    feat: страница одного теста - скелетон если тест грузится

--- src/views/single-test/ui/single-test-page.tsx
+++ src/views/single-test/ui/single-test-page.tsx
@@ -1,14 +1,16 @@
+import { TestForm } from '@/features/test-form'
+import { SkeletonTestCard } from '@/features/test-form/ui/skeleton-test-card'
 import { Typography } from '@/shared/ui/typography'
 import { TestsList } from '@/widgets/tests-list'
 
 const SingleTestPage = ({ title }: { title?: string }) => {
+  const test = undefined
   return (
-    <section className="max-mobile:py-6 py-12">
-      <Typography tag="h1" variant="visuelt-bold-48" className="mb-6">
+    <section className="max-mobile:py-6 mb-12 flex flex-col gap-y-10 py-12">
+      <Typography tag="h1" variant="visuelt-bold-48">
         {title}
       </Typography>
-      {/* <TestForm test={test} /> */}
-      <p>!!!!!!!! Вставить тест !!!!!</p>
+      {test! ? <TestForm test={undefined} /> : <SkeletonTestCard />}
       <TestsList title="Так же рекомендуем" />
     </section>
   )
