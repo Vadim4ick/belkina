2ad83ea2353a316c64da8c46724b4f73a43e7aa1
Vadim <a-yva-2013@mail.ru>
2025-06-30 22:36:10 +0400

    восставновление

--- src/entities/test/model/schema.ts
+++ src/entities/test/model/schema.ts
@@ -2,32 +2,36 @@ import { QuestionFragmentFragment } from '@/shared/graphql/__generated__'
 import { z } from 'zod'
 
 export const createStepSchema = (question: QuestionFragmentFragment) => {
+  const key = `q_${question.id}`
+
   switch (question.questionType) {
     case 'text_input':
       return z.object({
-        [`q_${question.id}`]: z.string().min(1, 'Обязательное поле'),
+        [key]: z.string().min(1, 'Обязательное поле'),
       })
 
     case 'single_choice':
       return z.object({
-        [`q_${question.id}`]: z.string().min(1, 'Выберите вариант'),
+        [key]: z.string().min(1, 'Выберите вариант'),
       })
 
     case 'multiple_choice':
       return z.object({
-        [`q_${question.id}`]: z.array(z.string()).min(1, 'Выберите хотя бы один вариант'),
+        [key]: z.array(z.string()).min(1, 'Выберите хотя бы один вариант'),
       })
 
     // case 'matching':
     //   return z.object({
-    //     [`q_${question.id}`]: z
-    //       .record(z.string())
-    //       .refine((obj) => Object.keys(obj).length === question.matchingPairs.length, {
-    //         message: 'Заполните все поля',
-    //       })
-    //       .refine((obj) => Object.values(obj).every((v) => v.trim() !== ''), {
-    //         message: 'Заполните все поля',
-    //       }),
+    //     [key]: z.object({
+    //       userAnswer: z
+    //         .record(z.string().regex(/^\d+$/, 'Ответ должен быть числом'))
+    //         .refine((obj) => Object.keys(obj).length === question.matchingPairs.length, {
+    //           message: 'Заполните все поля',
+    //         })
+    //         .refine((obj) => Object.values(obj).every((v) => v.trim() !== ''), {
+    //           message: 'Заполните все поля',
+    //         }),
+    //     }),
     //   })
 
     default:
--- src/features/test-form/model/const.ts
+++ src/features/test-form/model/const.ts
@@ -1,7 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-export function formatUserAnswer(type: string, raw: any): string | string[] {
-  if (type === 'multiple_choice' || type === 'single_choice') {
+export function formatUserAnswer(
+  type: 'single_choice' | 'multiple_choice' | 'text_input' | 'matching',
+  raw: any,
+): string | string[] {
+  if (type === 'multiple_choice' || type === 'single_choice' || type === 'text_input') {
     return Array.isArray(raw) ? raw.map(String) : [String(raw)]
   }
+
+  if (type === 'matching') {
+    // допустим, raw — объект вроде { A: '1', B: '3' }
+    // ты можешь сохранить это как JSON-строку или как строку формата "A1 B3"
+    return JSON.stringify(raw)
+  }
+
+  // single_choice и text_input → строка
   return typeof raw === 'string' ? raw : String(raw)
 }
