236aa9cf3e24febdda02e8a06e05a80f5727be4b
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-07-01 18:29:27 +0300

    feat: add post collection and render archive on blog page

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,31 @@
 import { withPayload } from '@payloadcms/next/withPayload'
+import redirects from './redirects.js'
+
+const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
-  reactStrictMode: false,
   images: {
     remotePatterns: [
+      ...NEXT_PUBLIC_SERVER_URL.split(',').map((item) => {
+        const url = new URL(item.trim())
+
+        return {
+          protocol: url.protocol.replace(':', ''), // 'http' or 'https'
+          hostname: url.hostname, // 'localhost'
+          port: url.port, // optional
+          pathname: '/api/media/file/**', // adjust to match your image path
+        }
+      }),
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-        port: '',
         pathname: '/**',
       },
     ],
   },
+  reactStrictMode: true,
+  redirects,
 }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
