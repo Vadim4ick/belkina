844a5b4a79039e939d23141accd521f47862d557
Vadim Firulyov <firulvv@mail.ru>
2025-07-03 21:37:52 +0400

    ✨ (Courses): добавлена новая коллекция "Курсы" с полями для управления курсами 📝 (KinescopeVideoSelect): обновлен компонент выбора видео Kinescope 📝 (importMap): обновлен импорт для нового компонента KinescopeVideoSelect Добавлена новая коллекция "Курсы", которая позволяет управлять курсами, включая заголовок, описание, баннер, экзамены, предметы и видео из Kinescope. Обновлен компонент KinescopeVideoSelect для поддержки новых полей, таких как заголовок и длительность видео, что улучшает функциональность и пользовательский интерфейс.

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -4,6 +4,7 @@ import { LexicalDiffComponent as LexicalDiffComponent_44fe37237e0ebf4470c9990d8c
 import { FixedToolbarFeatureClient as FixedToolbarFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
 import { UnorderedListFeatureClient as UnorderedListFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
 import { HeadingFeatureClient as HeadingFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { default as default_1ca3e7932c9575e5beb2cc460d4b6a0d } from '@/app/(payload)/components/fields/KinescopeVideoSelect'
 
 export const importMap = {
   "@payloadcms/richtext-lexical/rsc#RscEntryLexicalCell": RscEntryLexicalCell_44fe37237e0ebf4470c9990d8cb7b07e,
@@ -11,5 +12,6 @@ export const importMap = {
   "@payloadcms/richtext-lexical/rsc#LexicalDiffComponent": LexicalDiffComponent_44fe37237e0ebf4470c9990d8cb7b07e,
   "@payloadcms/richtext-lexical/client#FixedToolbarFeatureClient": FixedToolbarFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
   "@payloadcms/richtext-lexical/client#UnorderedListFeatureClient": UnorderedListFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
-  "@payloadcms/richtext-lexical/client#HeadingFeatureClient": HeadingFeatureClient_e70f5e05f09f93e00b997edb1ef0c864
+  "@payloadcms/richtext-lexical/client#HeadingFeatureClient": HeadingFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@/app/(payload)/components/fields/KinescopeVideoSelect#default": default_1ca3e7932c9575e5beb2cc460d4b6a0d
 }
