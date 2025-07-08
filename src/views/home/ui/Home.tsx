4f850c5235d7e341dca8783e410523a9f3cd57f3
Vadim Firulyov <firulvv@mail.ru>
2025-07-08 12:42:02 +0400

    🐛 (page.tsx, Home.tsx): замена Promise.allSettled на прямые вызовы функций gql ✨ (page.tsx, Home.tsx): упрощение обработки результатов запросов Упрощена логика обработки результатов запросов к GraphQL, что улучшает читаемость кода и уменьшает количество лишних проверок. Теперь код более прямолинеен, что облегчает его поддержку и понимание.

--- src/views/home/ui/Home.tsx
+++ src/views/home/ui/Home.tsx
@@ -3,12 +3,10 @@ import { TestsBanner } from './home-banners/tests-banner'
 import { AboutBanner } from './home-banners/about-banner'
 import { AskedQuestions } from '@/features/asked-questions'
 import { TestCardQuestions } from '@/widgets/test-card-questions'
-import { getServerGqlClient } from '@/shared/graphql/client'
+import { gql } from '@/shared/graphql/client'
 import { getSettledValue } from '@/shared/lib/utils'
 
 const Home = async () => {
-  const gql = await getServerGqlClient()
-
   const [res, faqs] = await Promise.allSettled([gql.GetHomePage(), gql.GetFAGs()])
 
   const resVal = getSettledValue(res)
