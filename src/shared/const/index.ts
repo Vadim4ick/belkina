8be1c2f592d4f0212ccb27726d0837de539b556a
Vadim Firulyov <firulvv@mail.ru>
2025-07-09 00:22:42 +0400

    fix navbar

--- src/shared/const/index.ts
+++ src/shared/const/index.ts
@@ -1,3 +1,10 @@
+import { GraduationCapIcon } from '@/shared/icons/graduation-cap'
+import { BookOpennCapIcon } from '@/shared/icons/book-open-text'
+import { FAQIcon } from '@/shared/icons/file-question-mark'
+import { Newspaper } from '@/shared/icons/newspaper'
+import { getRouteHome, getRouteCourses, getRoutePosts } from '@/shared/lib/routes'
+import { IHeaderItems } from '@/widgets/app-header'
+
 export const RUS_LETTERS = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'.split('')
 
 export function getSymbolLabel(count: number): string {
@@ -12,3 +19,26 @@ export function getSymbolLabel(count: number): string {
       return `${count} символов`
   }
 }
+
+export const headerItems: IHeaderItems[] = [
+  {
+    title: 'Главная',
+    url: getRouteHome(),
+    icon: GraduationCapIcon,
+  },
+  {
+    title: 'Курсы',
+    url: getRouteCourses(),
+    icon: BookOpennCapIcon,
+  },
+  {
+    title: 'FAQ',
+    url: '#',
+    icon: FAQIcon,
+  },
+  {
+    title: 'Блог',
+    url: getRoutePosts(),
+    icon: Newspaper,
+  },
+]
