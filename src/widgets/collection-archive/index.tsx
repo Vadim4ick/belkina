7f242575b228332f20fc0f4af6775b09ea90812e
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-07-09 13:08:52 +0300

    посты: переиспользован компонен product card

--- src/widgets/collection-archive/index.tsx
+++ src/widgets/collection-archive/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
 import { cn } from '@/shared/lib/utils'
-import { Card } from '@/entities/post/ui/post-card'
 import { GetPostListQuery } from '@/shared/graphql/__generated__'
+import { ProductCard } from '../product-card'
+import { ProductCardsGridCatalog } from '../product-cards-grid-catalog'
 
 export type Props = {
   posts: GetPostListQuery['Posts']['docs']
@@ -13,20 +14,19 @@ export const CollectionArchive: React.FC<Props> = (props) => {
 
   return (
     <div className={cn('container')}>
-      <div>
-        <div className="grid grid-cols-4 gap-x-4 gap-y-4 sm:grid-cols-8 lg:grid-cols-12 lg:gap-x-8 lg:gap-y-8 xl:gap-x-8">
-          {posts?.map((result, index) => {
-            if (typeof result === 'object' && result !== null) {
-              return (
-                <div className="col-span-4" key={index}>
-                  {result.slug && <Card className="h-[400px]" doc={result} relationTo="posts" />}
-                </div>
-              )
-            }
-            return null
-          })}
-        </div>
-      </div>
+      <ProductCardsGridCatalog isNull={posts.length === 0} title="Архив статей">
+        {posts &&
+          posts?.map((product) => (
+            <ProductCard
+              key={product.slug}
+              title={product.title}
+              description={product.meta.description}
+              image={product.meta.image}
+              url={`posts/${product.slug}`}
+              showFooter={false}
+            />
+          ))}
+      </ProductCardsGridCatalog>
     </div>
   )
 }
