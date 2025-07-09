5d2ca05cbe1bf0897230bae8c783c58eab53cc14
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-07-09 11:54:36 +0300

    heder: навигация помещена в контейнер

--- src/widgets/app-header/index.tsx
+++ src/widgets/app-header/index.tsx
@@ -17,6 +17,7 @@ import { Newspaper } from '@/shared/icons/newspaper'
 import { MenuIcon, XIcon } from 'lucide-react'
 import { UserProfile } from './ui/user-profile'
 import { MobileNavButton } from './ui/mobile-nav-button'
+import { Container } from '@/shared/ui/container'
 
 interface NavItem {
   title: string
@@ -78,85 +79,87 @@ export const AppHeader = ({ route }: AppHeaderProps) => {
         isScrolled ? 'py-2 shadow-sm' : 'py-4',
       )}
     >
-      <div className="mx-auto flex items-center justify-between">
-        <div className="flex items-center gap-x-8">
-          {/* Мобильное меню (бургер) */}
-          <button
-            className="z-50 p-2 lg:hidden"
-            onClick={() => setIsOpen(!isOpen)}
-            aria-label="Меню"
-          >
-            {isOpen ? <XIcon className="h-6 w-6" /> : <MenuIcon className="h-6 w-6" />}
-          </button>
-          {/* Логотип */}
-          <Logo className="hidden sm:flex" />
-        </div>
-
-        {/* Десктопное меню */}
-        <nav className="hidden items-center gap-8 lg:flex">
-          {navItems.map((item) => (
-            <Link
-              key={item.url}
-              href={item.url}
-              className={cn(
-                `hover:text-blue-hover font-medium transition-colors`,
-                pathname === item.url ? 'text-blue' : 'text-dark-grey',
-              )}
+      <Container>
+        <div className="mx-auto flex items-center justify-between">
+          <div className="flex items-center gap-x-8">
+            {/* Мобильное меню (бургер) */}
+            <button
+              className="z-50 p-2 lg:hidden"
+              onClick={() => setIsOpen(!isOpen)}
+              aria-label="Меню"
             >
-              <Typography variant="poppins-md-16" tag="p">
-                {item.title}
-              </Typography>
-            </Link>
-          ))}
-        </nav>
+              {isOpen ? <XIcon className="h-6 w-6" /> : <MenuIcon className="h-6 w-6" />}
+            </button>
+            {/* Логотип */}
+            <Logo className="hidden sm:flex" />
+          </div>
 
-        {/* Кнопки авторизации/профиля */}
-        <div className="flex items-center gap-4">
-          {!session && (
-            <Button variant="secondary" onClick={() => console.log('Записаться')}>
-              Записаться на урок
-            </Button>
-          )}
-          <UserProfile session={session} status={status} />
-        </div>
-
-        {/* Мобильное меню (контент) */}
-        <div
-          className={cn(
-            'bg-background fixed inset-0 z-40 max-w-[90%] transform pt-20 transition-all duration-500 md:w-[550px]',
-            'lg:hidden',
-            isOpen ? 'translate-x-0' : '-translate-x-full',
-          )}
-        >
-          <div className="bg-background flex h-dvh w-full flex-col space-y-2 px-4">
-            <Logo className="mb-5" />
+          {/* Десктопное меню */}
+          <nav className="hidden items-center gap-8 lg:flex">
             {navItems.map((item) => (
-              <Link key={item.url} href={item.url}>
-                <MobileNavButton
-                  variant={pathname === item.url ? 'secondary' : 'ghostWhite'}
-                  icon={item.icon}
-                >
-                  <span>{item.title}</span>
-                </MobileNavButton>
+              <Link
+                key={item.url}
+                href={item.url}
+                className={cn(
+                  `hover:text-blue-hover font-medium transition-colors`,
+                  pathname === item.url ? 'text-blue' : 'text-dark-grey',
+                )}
+              >
+                <Typography variant="poppins-md-16" tag="p">
+                  {item.title}
+                </Typography>
               </Link>
             ))}
+          </nav>
 
-            <div className="space-y-5 border-t pt-4">
-              {!session && (
-                <Button className="h-12 w-full" onClick={() => console.log('Записаться')}>
-                  Записаться на урок
-                </Button>
-              )}
-              <UserProfile
-                session={session}
-                status={status}
-                reverse={true}
-                className="h-12 w-full"
-              />
+          {/* Кнопки авторизации/профиля */}
+          <div className="flex items-center gap-4">
+            {!session && (
+              <Button variant="secondary" onClick={() => console.log('Записаться')}>
+                Записаться на урок
+              </Button>
+            )}
+            <UserProfile session={session} status={status} />
+          </div>
+
+          {/* Мобильное меню (контент) */}
+          <div
+            className={cn(
+              'bg-background fixed inset-0 z-40 max-w-[90%] transform pt-20 transition-all duration-500 md:w-[550px]',
+              'lg:hidden',
+              isOpen ? 'translate-x-0' : '-translate-x-full',
+            )}
+          >
+            <div className="bg-background flex h-dvh w-full flex-col space-y-2 px-4">
+              <Logo className="mb-5" />
+              {navItems.map((item) => (
+                <Link key={item.url} href={item.url}>
+                  <MobileNavButton
+                    variant={pathname === item.url ? 'secondary' : 'ghostWhite'}
+                    icon={item.icon}
+                  >
+                    <span>{item.title}</span>
+                  </MobileNavButton>
+                </Link>
+              ))}
+
+              <div className="space-y-5 border-t pt-4">
+                {!session && (
+                  <Button className="h-12 w-full" onClick={() => console.log('Записаться')}>
+                    Записаться на урок
+                  </Button>
+                )}
+                <UserProfile
+                  session={session}
+                  status={status}
+                  reverse={true}
+                  className="h-12 w-full"
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </Container>
     </header>
   )
 }
