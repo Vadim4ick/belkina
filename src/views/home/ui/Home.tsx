4f7b73731ab7507867d71e03a5319790d4fad897
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-07-10 19:25:44 +0300

    feat: карусель для табличек (драфт)

--- src/views/home/ui/Home.tsx
+++ src/views/home/ui/Home.tsx
@@ -5,12 +5,18 @@ import { AskedQuestions } from '@/features/asked-questions'
 import { TestCardQuestions } from '@/widgets/test-card-questions'
 import { gql } from '@/shared/graphql/client'
 import { getSettledValue } from '@/shared/lib/utils'
+import { SliderWrapper } from '@/widgets/slider-wrapper'
 
 const Home = async () => {
-  const [res, faqs] = await Promise.allSettled([gql.GetHomePage(), gql.GetFAGs()])
+  const [res, faqs, posts] = await Promise.allSettled([
+    gql.GetHomePage(),
+    gql.GetFAGs(),
+    gql.GetPostList({ limit: 9, page: 1 }),
+  ])
 
   const resVal = getSettledValue(res)
   const faqsVal = getSettledValue(faqs)
+  const postssVal = getSettledValue(posts)
 
   return (
     <>
@@ -19,6 +25,7 @@ const Home = async () => {
       <TestCardQuestions test={resVal?.HomePage.featuredTest} />
       <AskedQuestions faqs={faqsVal?.Faqs.docs} />
       <TestsBanner content={resVal?.HomePage.diagnosticTestBanner} />
+      {postssVal && <SliderWrapper posts={postssVal?.Posts} />}
     </>
   )
 }
