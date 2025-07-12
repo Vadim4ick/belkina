445dc32a8f5993b6d47ecfca124e3d32d56a38c6
Vadim Firulyov <firulvv@mail.ru>
2025-07-12 18:04:44 +0400

    fix log

--- src/widgets/app-header/index.tsx
+++ src/widgets/app-header/index.tsx
@@ -20,8 +20,6 @@ export const AppHeader = () => {
   const pathname = usePathname()
   const { data: session, status } = useSession()
 
-  console.log(session?.tokens)
-
   useEffect(() => {
     const handleScroll = () => {
       setIsScrolled(window.scrollY > 50)
