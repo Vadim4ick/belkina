ac25873e89f412196c449d2fa843325b772e97d7
Vadim <a-yva-2013@mail.ru>
2025-07-12 23:52:55 +0400

    ✨ (auth.ts): добавлена опция trustHost в конфигурацию аутентификации ♻️ (client.ts): удален код, связанный с загрузкой переменных окружения и заменен на прямое использование process.env Изменение в auth.ts добавляет возможность доверять хосту, что может улучшить безопасность аутентификации. В client.ts удален избыточный код для загрузки переменных окружения, что упрощает код и делает его более читаемым, а также позволяет использовать переменные окружения напрямую.

--- src/shared/graphql/client.ts
+++ src/shared/graphql/client.ts
@@ -2,18 +2,6 @@ import { GraphQLClient } from 'graphql-request'
 import * as generated from './__generated__'
 import { auth } from '@/entities/user/auth'
 
-// import dotenv from 'dotenv'
-
-// dotenv.config({
-//   path: '.env',
-// })
-
-export const PAYLOAD_URL: string = (() => {
-  const url = process.env.NEXT_PUBLIC_PAYLOAD_GRAPHQL
-  if (!url) throw new Error('Environment variable NEXT_PUBLIC_PAYLOAD_GRAPHQL is not set')
-  return url
-})()
-
 export const nextFetchWithTags = (tags: string[]) => {
   const nextFetch = (input: RequestInfo, init?: RequestInit) => {
     return fetch(input, {
@@ -30,7 +18,7 @@ export const createGqlClient = ({ token, tags }: { token?: string; tags?: string
   // if (Object.keys(generated).length === 0) return
 
   return generated.getSdk(
-    new GraphQLClient(PAYLOAD_URL, {
+    new GraphQLClient(process.env.NEXT_PUBLIC_PAYLOAD_GRAPHQL!, {
       fetch: tags && nextFetchWithTags(tags || []),
       headers: token ? { Authorization: `Bearer ${token}` } : {},
     }),
