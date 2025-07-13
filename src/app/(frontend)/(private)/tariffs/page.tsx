ac07e62a2ed054c20abd23e0e9c54bdfa93e3b8e
Vadim Firulyov <firulvv@mail.ru>
2025-07-13 12:33:34 +0400

    Добавление страницы тарифов

--- src/app/(frontend)/(private)/tariffs/page.tsx
+++ src/app/(frontend)/(private)/tariffs/page.tsx
@@ -1,3 +1,12 @@
-export default function Page() {
-  return <div>Тарифы</div>
+import { getServerAuthGqlClient } from '@/shared/graphql/client'
+import { TariffsPage } from '@/views/tariffs'
+
+export const revalidate = 180
+
+export default async function Page() {
+  const gql = await getServerAuthGqlClient({})
+
+  const tariffs = await gql.GetTaraffis()
+
+  return <TariffsPage tariffs={tariffs?.Tariffs.docs} />
 }
