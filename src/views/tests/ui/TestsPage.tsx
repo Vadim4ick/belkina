66e247591c31ddfb7bab26efc61edd451550a068
Vadim <a-yva-2013@mail.ru>
2025-07-14 17:14:49 +0400

    ✨ (dialog.tsx, filter-category): добавлены компоненты диалогового окна и фильтров ✨ (courses-list.tsx, tests-page.tsx): интеграция новых фильтров в списки курсов и тестов Созданы новые компоненты для диалогового окна и фильтров, что улучшает пользовательский интерфейс и позволяет пользователям более удобно фильтровать курсы и тесты. Это упрощает управление состоянием фильтров и улучшает взаимодействие с пользователем.

--- src/views/tests/ui/TestsPage.tsx
+++ src/views/tests/ui/TestsPage.tsx
@@ -1,13 +1,13 @@
 'use client'
 
-import { TabCategory } from '@/features/tab-categories'
 import { useGetAllUserTests } from '@/shared/services/test.service'
 import { Typography } from '@/shared/ui/typography'
 import { TestsList } from '@/widgets/tests-list'
 import { memo } from 'react'
-import { btnsCategoryTests, MAPPING_TEST_CATEGORY } from '../model/const'
+import { MAPPING_TEST_CATEGORY } from '../model/const'
 import { GetAllExamsQuery, GetAllSubjectsQuery } from '@/shared/graphql/__generated__'
 import { useTestsStore } from '@/entities/test/model/use-tests-store'
+import { FilterTests } from '@/widgets/filter-category'
 
 const TestsPage = memo(
   ({
@@ -17,7 +17,7 @@ const TestsPage = memo(
     exams?: GetAllExamsQuery['Exams']['docs']
     subjects?: GetAllSubjectsQuery['Subjects']['docs']
   }) => {
-    const { filters, setFilter, setCategoryIdx } = useTestsStore()
+    const { filters } = useTestsStore()
 
     const { data: res, isLoading } = useGetAllUserTests({
       status: MAPPING_TEST_CATEGORY[filters.categoryIdx],
@@ -28,31 +28,12 @@ const TestsPage = memo(
     return (
       <section className="max-mobile:py-6 py-12">
         <div>
-          <div className="max-tablet:flex-col max-tablet:items-start mb-6 flex items-center justify-between gap-2">
+          <div className="flex items-center justify-between gap-2">
             <Typography tag="h1" variant="visuelt-bold-48">
               Тесты
             </Typography>
 
-            <TabCategory
-              btns={btnsCategoryTests}
-              value={filters.categoryIdx}
-              onChange={(val) => setCategoryIdx(val)}
-            />
-          </div>
-
-          <div className="flex flex-col gap-4">
-            <TabCategory
-              btns={[{ id: 1000, title: 'Все' }, ...(exams?.map((el) => el) || [])]}
-              value={filters.examId}
-              onChange={(val) => setFilter('examId', val)}
-              variant="secondary"
-            />
-            <TabCategory
-              btns={[{ id: 1000, title: 'Все' }, ...(subjects?.map((el) => el) || [])]}
-              value={filters.subjectId}
-              onChange={(val) => setFilter('subjectId', val)}
-              variant="secondary"
-            />
+            <FilterTests exams={exams} subjects={subjects} />
           </div>
         </div>
 
