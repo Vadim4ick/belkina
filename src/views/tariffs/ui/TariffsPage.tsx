64d42dc550fa183b2ef04e1392b6993ab482c438
Vadim Firulyov <firulvv@mail.ru>
2025-07-17 21:19:10 +0400

    ♻️ (topic.tsx, tariffs): удаление неиспользуемого кода и компонентов 📝 (tariffs): удаление страницы тарифов и связанных с ней компонентов Упрощение кода за счет удаления неиспользуемых компонентов и страниц, что улучшает читаемость и поддерживаемость проекта. Удаление страницы тарифов связано с изменениями в функциональности приложения, что позволяет сосредоточиться на более актуальных частях кода.

--- src/views/tariffs/ui/TariffsPage.tsx
+++
@@ -1,8 +0,0 @@
-import { TariffList } from '@/features/tariff-list'
-import { GetTaraffisQuery } from '@/shared/graphql/__generated__'
-
-const TariffsPage = ({ tariffs }: { tariffs: GetTaraffisQuery['Tariffs']['docs'] }) => {
-  return <TariffList tarrifs={tariffs} />
-}
-
-export { TariffsPage }
