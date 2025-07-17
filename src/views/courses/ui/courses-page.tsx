98a3caef4e567e163600290f6c3f374ad748b001
Vadim <a-yva-2013@mail.ru>
2025-07-17 11:59:35 +0400

    ♻️ (courses-page.tsx): удалены неиспользуемые импорты и переменные, упрощен код Изменения упрощают компонент CoursesPage, удаляя неиспользуемый импорт TariffList и переменную tarrifs, что делает код более чистым и понятным. Это также улучшает производительность, так как уменьшается количество асинхронных вызовов, необходимых для получения данных.

--- src/views/courses/ui/courses-page.tsx
+++ src/views/courses/ui/courses-page.tsx
@@ -1,4 +1,3 @@
-import { TariffList } from '@/features/tariff-list'
 import { CoursesList } from './courses-list'
 import { getSettledValue } from '@/shared/lib/utils'
 import { getServerAuthGqlClient } from '@/shared/actions/getServerAuthGqlClient'
@@ -6,23 +5,12 @@ import { getServerAuthGqlClient } from '@/shared/actions/getServerAuthGqlClient'
 const CoursesPage = async () => {
   const gql = await getServerAuthGqlClient({})
 
-  const [exams, subjects, tarrifs] = await Promise.allSettled([
-    gql.GetAllExams(),
-    gql.GetAllSubjects(),
-    gql.GetTaraffis(),
-  ])
+  const [exams, subjects] = await Promise.allSettled([gql.GetAllExams(), gql.GetAllSubjects()])
 
   const examsVal = getSettledValue(exams)
   const subjectsVal = getSettledValue(subjects)
-  const tarrifsVal = getSettledValue(tarrifs)
 
-  return (
-    <>
-      <CoursesList exams={examsVal?.Exams.docs} subjects={subjectsVal?.Subjects.docs} />
-
-      <TariffList tarrifs={tarrifsVal?.Tariffs.docs} />
-    </>
-  )
+  return <CoursesList exams={examsVal?.Exams.docs} subjects={subjectsVal?.Subjects.docs} />
 }
 
 export { CoursesPage }
