043e011155d141b5de2ec57fe118fd60eecb0b1f
Vadim <a-yva-2013@mail.ru>
2025-07-21 22:46:22 +0400

    ✨ (auth/sign-in/page.tsx): добавлена поддержка useQueryClient для обновления кэша после входа ✨ (HomePage.ts): добавлено обновление кэша для домашней страницы после изменений ✨ (posts/index.ts): добавлено обновление кэша для домашней страницы при изменении постов ♻️ (redis/client.ts): закомментированы логи подключения и ошибок Redis
    Добавление поддержки useQueryClient в компоненте входа позволяет обновлять кэш после успешного входа пользователя. Обновление кэша для домашней страницы после изменений в других частях приложения улучшает согласованность данных. Закомментированные логи подключения Redis могут помочь избежать избыточного вывода в консоль, сохраняя при этом возможность отладки при необходимости.

--- src/shared/redis/client.ts
+++ src/shared/redis/client.ts
@@ -4,7 +4,7 @@ export const redis = createClient({
   url: process.env.REDIS_URL,
 })
 
-redis.on('connect', () => console.log('✅ Redis connected'))
-redis.on('error', console.error)
+// redis.on('connect', () => console.log('✅ Redis connected'))
+// redis.on('error', console.error)
 
 redis.connect().catch(console.error)
