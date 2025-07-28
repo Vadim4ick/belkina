e805ed8b6429d4dcb0c7da8f16e6ee6cafe08e7c
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-07-28 22:34:48 +0300

    fiat: добавлена страница и роут vebinars. Слой во views не создавал

--- src/app/(frontend)/(public)/courses/page.tsx
+++ src/app/(frontend)/(public)/courses/page.tsx
@@ -1,13 +1,7 @@
 import { CoursesPage } from '@/views/courses'
-import { WebinarCalendar } from '@/widgets/webinar-calendar'
 
 export const revalidate = 180
 
 export default function Page() {
-  return (
-    <>
-      {/* <CoursesPage /> */}
-      <WebinarCalendar />
-    </>
-  )
+  return <CoursesPage />
 }
--- src/shared/const/index.ts
+++ src/shared/const/index.ts
@@ -2,7 +2,7 @@ import { GraduationCapIcon } from '@/shared/icons/graduation-cap'
 import { BookOpennCapIcon } from '@/shared/icons/book-open-text'
 import { FAQIcon } from '@/shared/icons/file-question-mark'
 import { Newspaper } from '@/shared/icons/newspaper'
-import { getRouteHome, getRouteCourses, getRoutePosts } from '@/shared/lib/routes'
+import { getRouteHome, getRouteCourses, getRoutePosts, getRouteVebinars } from '@/shared/lib/routes'
 import { TestResult_Status_All } from '../graphql/__generated__'
 
 export const RUS_LETTERS = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'.split('')
@@ -47,6 +47,11 @@ export const navItems: NavItemsProps[] = [
   {
     title: 'Курсы',
     url: getRouteCourses(),
+    icon: BookOpennCapIcon,
+  },
+  {
+    title: 'Вебинары',
+    url: getRouteVebinars(),
     icon: BookOpennCapIcon,
   },
   {
