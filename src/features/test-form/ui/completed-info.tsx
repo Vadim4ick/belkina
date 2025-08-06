1111a8e033a416e5c4e2bcd2d9c2f61ec30c66c3
Vadim <a-yva-2013@mail.ru>
2025-08-06 22:12:56 +0400

    ✨ (completed-info.tsx): добавлены новые функции для отображения результатов теста ✨ (utils.ts): добавлена функция getResultLevel для оценки результатов 🐛 (test-form.tsx): исправлена логика загрузки для отображения состояния Изменения улучшают пользовательский интерфейс, предоставляя более детальную информацию о результатах теста, включая процент правильных ответов и уровень результата. Это позволяет пользователям лучше понять свои достижения и получить рекомендации по дальнейшему обучению. Исправление логики загрузки предотвращает ненужное отображение состояния загрузки, когда результаты теста отсутствуют.

--- src/features/test-form/ui/completed-info.tsx
+++ src/features/test-form/ui/completed-info.tsx
@@ -1,7 +1,9 @@
 import { useProfileStore } from '@/entities/user/use-profile-store'
+import { getRouteWebinars } from '@/shared/lib/routes'
+import { getResultLevel } from '@/shared/lib/utils'
 import { Button } from '@/shared/ui/button'
-import { Input } from '@/shared/ui/input'
 import { Typography } from '@/shared/ui/typography'
+import Link from 'next/link'
 import { memo } from 'react'
 
 const CompletedInfo = memo(
@@ -20,41 +22,79 @@ const CompletedInfo = memo(
   }) => {
     const { profile } = useProfileStore()
 
+    const percent = Math.round((totalCorrectAnswers / countQuestions) * 100)
+
+    const score = publicFlag ? publicCorrectAnswers : totalCorrectAnswers
+    const resultLevel = getResultLevel(percent)
+
     if (publicFlag || !!!profile?.id) {
       return (
         <div className="mx-auto flex max-w-md flex-col items-center justify-center gap-8 rounded-2xl border bg-white p-10 text-center shadow-lg">
           <div className="text-4xl">🎉</div>
 
-          <Typography tag="h2" variant="poppins-md-16" className="font-semibold text-green-400">
-            Вы успешно прошли тест! <br />
-            {`Количество правильных ответов: ${publicCorrectAnswers} из ${countQuestions}`}
-          </Typography>
+          <div className="flex flex-col items-center justify-center gap-1">
+            <Typography tag="p" variant="poppins-md-16" className="font-semibold">
+              <b>Правильных ответов:</b> {score} из {countQuestions}
+            </Typography>
+            <p>
+              <b>Результат:</b> {percent}%
+            </p>
+            <p>
+              <b>{resultLevel}</b>
+            </p>
+          </div>
 
-          <Typography tag="p" variant="poppins-md-16" className="text-dark-grey">
-            Для получения бесплатного видеоурока укажите свой e-mail
-          </Typography>
-
-          <div className="flex w-full flex-col gap-4">
-            <Input type="email" name="email" placeholder="Ваш e-mail" />
-
-            <Button>Отправить</Button>
+          <div className="flex list-disc flex-col items-start gap-0.5">
+            <Button size={'sm'} type="button" variant={'ghostWhite'}>
+              <Link href={getRouteWebinars()}>🎓 Записаться на живой разбор заданий</Link>
+            </Button>
           </div>
         </div>
       )
     }
 
     return (
-      <div className="mx-auto flex max-w-md flex-col items-center justify-center gap-8 p-10 text-center">
-        <div className="text-4xl">🎉</div>
-
-        <Typography tag="h2" variant="poppins-md-16" className="font-semibold text-green-400">
-          Вы успешно прошли тест! <br />
-          {`Количество правильных ответов: ${totalCorrectAnswers} из ${countQuestions}`}
+      <div className="mx-auto w-full bg-white">
+        <Typography tag="h2" variant="poppins-md-24" className="mb-4 text-left">
+          🎉 Поздравляем! Вы прошли тест.
         </Typography>
 
-        <Button onClick={resetTestRes} type="button">
-          Пройти снова
-        </Button>
+        <ul className="mb-6 ml-4 list-disc text-left">
+          <li>
+            <Typography tag="p" variant="poppins-md-16" className="font-semibold">
+              <b>Правильных ответов:</b> {score} из {countQuestions}
+            </Typography>
+          </li>
+
+          <li>
+            <b>Результат:</b> {percent}% — <b>{resultLevel}</b>
+          </li>
+        </ul>
+
+        <Typography tag="h3" variant="poppins-md-16" className="mb-2 text-left">
+          Рекомендации:
+        </Typography>
+
+        <ul className="mb-6 ml-4 list-disc text-left">
+          <li>Вы хорошо ориентируетесь в теме — продолжайте развиваться в том же духе!</li>
+          <li>
+            Мы рекомендуем пройти <b>задание 4</b> для закрепления тем, в которых были ошибки.
+          </li>
+        </ul>
+
+        <Typography tag="h3" variant="poppins-md-16" className="mb-2 text-left">
+          Что дальше?
+        </Typography>
+
+        <div className="flex list-disc flex-col items-start gap-0.5">
+          <Button size={'sm'} type="button" variant={'ghostWhite'} onClick={resetTestRes}>
+            🌀 Пройти тест ещё раз
+          </Button>
+
+          <Button size={'sm'} type="button" variant={'ghostWhite'}>
+            <Link href={getRouteWebinars()}>🎓 Записаться на живой разбор заданий</Link>
+          </Button>
+        </div>
       </div>
     )
   },
