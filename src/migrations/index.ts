78214db6bc8d45d3af40b8f2c08674f5c159cd70
Vadim Firulyov <firulvv@mail.ru>
2025-08-14 18:26:16 +0400

    изменения

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250814_142153 from './20250814_142153';
+
+export const migrations = [
+  {
+    up: migration_20250814_142153.up,
+    down: migration_20250814_142153.down,
+    name: '20250814_142153'
+  },
+];
