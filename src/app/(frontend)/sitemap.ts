e4ec41c2e25ba8cbce2475598bd637255f08cb76
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-08-15 22:58:05 +0300

    robots tsxt, sitemap, yandex counter

---
+++ src/app/(frontend)/sitemap.ts
@@ -0,0 +1,52 @@
+import type { MetadataRoute } from 'next'
+
+const baseUrl = 'https://belkina.online'
+
+const sitemap: MetadataRoute.Sitemap = [
+  {
+    url: baseUrl + '/',
+    changeFrequency: 'weekly',
+    lastModified: new Date(),
+    priority: 1.0,
+  },
+  {
+    url: baseUrl + '/webinars',
+    changeFrequency: 'weekly',
+    lastModified: new Date(),
+    priority: 0.8,
+  },
+  {
+    url: baseUrl + '/faq',
+    changeFrequency: 'monthly',
+    lastModified: new Date(),
+    priority: 0.7,
+  },
+  {
+    url: baseUrl + '/tests',
+    changeFrequency: 'weekly',
+    lastModified: new Date(),
+    priority: 0.9,
+  },
+  {
+    url: baseUrl + '/blog',
+    changeFrequency: 'weekly',
+    lastModified: new Date(),
+    priority: 0.8,
+  },
+  {
+    url: baseUrl + '/support',
+    changeFrequency: 'monthly',
+    lastModified: new Date(),
+    priority: 0.5,
+  },
+  {
+    url: baseUrl + '/privacy-policy',
+    changeFrequency: 'yearly',
+    lastModified: new Date(),
+    priority: 0.3,
+  },
+]
+
+export default function sitemapFunc(): MetadataRoute.Sitemap {
+  return sitemap
+}
