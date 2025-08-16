46f39d5a65cdf2193b149df913cf022f92686f24
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-08-16 21:59:14 +0300

    fix: баг с миграцией 1

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20250814_142153 from './20250814_142153';
 import * as migration_20250815_110536 from './20250815_110536';
 import * as migration_20250816_175849_seo_field_for_home_page from './20250816_175849_seo_field_for_home_page';
+import * as migration_20250816_185455_seo_field_for_home_page_rename_to_meta from './20250816_185455_seo_field_for_home_page_rename_to_meta';
 
 export const migrations = [
   {
@@ -16,6 +17,11 @@ export const migrations = [
   {
     up: migration_20250816_175849_seo_field_for_home_page.up,
     down: migration_20250816_175849_seo_field_for_home_page.down,
-    name: '20250816_175849_seo_field_for_home_page'
+    name: '20250816_175849_seo_field_for_home_page',
+  },
+  {
+    up: migration_20250816_185455_seo_field_for_home_page_rename_to_meta.up,
+    down: migration_20250816_185455_seo_field_for_home_page_rename_to_meta.down,
+    name: '20250816_185455_seo_field_for_home_page_rename_to_meta'
   },
 ];
