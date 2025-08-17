34e32f69613018b28812ebc0796913c8c108bc2d
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-08-17 20:17:38 +0300

    + index

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250817_144404_initial_schema from './20250817_144404_initial_schema';
 import * as migration_20250817_150053_banner_desc_field from './20250817_150053_banner_desc_field';
+import * as migration_20250817_171354_mainOfferBanner_desc_field_renamed_to_description from './20250817_171354_mainOfferBanner_desc_field_renamed_to_description';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20250817_150053_banner_desc_field.up,
     down: migration_20250817_150053_banner_desc_field.down,
-    name: '20250817_150053_banner_desc_field'
+    name: '20250817_150053_banner_desc_field',
+  },
+  {
+    up: migration_20250817_171354_mainOfferBanner_desc_field_renamed_to_description.up,
+    down: migration_20250817_171354_mainOfferBanner_desc_field_renamed_to_description.down,
+    name: '20250817_171354_mainOfferBanner_desc_field_renamed_to_description'
   },
 ];
