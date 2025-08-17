1476a0055a7b1d70cecfd165e3ab686dd021a234
Vadim <a-yva-2013@mail.ru>
2025-08-17 18:45:24 +0400

    создание initial схемы миграции

--- src/migrations/20250817_124916_initial_schema.ts
+++
@@ -1,9 +0,0 @@
-import { type MigrateUpArgs, type MigrateDownArgs } from '@payloadcms/db-postgres'
-
-export async function up({}: MigrateUpArgs): Promise<void> {
-  // noop: база уже создана через dev-push
-}
-
-export async function down({}: MigrateDownArgs): Promise<void> {
-  // noop
-}
--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,21 +1,9 @@
-import * as migration_20250817_124916_initial_schema from './20250817_124916_initial_schema';
-import * as migration_20250817_133838_rename_desc_to_description from './20250817_133838_rename_desc_to_description';
-import * as migration_20250817_135625_sync_schema_snapshot from './20250817_135625_sync_schema_snapshot';
+import * as migration_20250817_144404_initial_schema from './20250817_144404_initial_schema';
 
 export const migrations = [
   {
-    up: migration_20250817_124916_initial_schema.up,
-    down: migration_20250817_124916_initial_schema.down,
-    name: '20250817_124916_initial_schema',
-  },
-  {
-    up: migration_20250817_133838_rename_desc_to_description.up,
-    down: migration_20250817_133838_rename_desc_to_description.down,
-    name: '20250817_133838_rename_desc_to_description',
-  },
-  {
-    up: migration_20250817_135625_sync_schema_snapshot.up,
-    down: migration_20250817_135625_sync_schema_snapshot.down,
-    name: '20250817_135625_sync_schema_snapshot'
+    up: migration_20250817_144404_initial_schema.up,
+    down: migration_20250817_144404_initial_schema.down,
+    name: '20250817_144404_initial_schema'
   },
 ];
