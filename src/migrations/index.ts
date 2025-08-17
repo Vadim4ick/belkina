539b1568a9d65cb9aedbab1f460c03045cc8413e
Vadim Firulyov <firulvv@mail.ru>
2025-08-17 16:56:47 +0400

    ✨ (deploy.yml, package.json, migrations): добавлены миграции и обновлен процесс сборки ♻️ (migrations): удалены устаревшие миграции и добавлены новые Обновлен процесс сборки в файле deploy.yml для использования команды `npm run ci`, которая включает миграции и сборку проекта. Удалены устаревшие миграции и добавлены новые, которые создают начальную схему базы данных, что улучшает структуру и управляемость базы данных.

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,16 +1,9 @@
-import * as migration_20250814_142153 from './20250814_142153'
-import * as migration_20250815_110536 from './20250815_110536'
-import * as migration_20250816_175849_seo_field_for_home_page from './20250816_175849_seo_field_for_home_page'
+import * as migration_20250817_124916_initial_schema from './20250817_124916_initial_schema';
 
 export const migrations = [
   {
-    up: migration_20250816_184352.up,
-    down: migration_20250816_184352.down,
-    name: '20250816_184352',
+    up: migration_20250817_124916_initial_schema.up,
+    down: migration_20250817_124916_initial_schema.down,
+    name: '20250817_124916_initial_schema'
   },
-  {
-    up: migration_20250816_175849_seo_field_for_home_page.up,
-    down: migration_20250816_175849_seo_field_for_home_page.down,
-    name: '20250816_175849_seo_field_for_home_page',
-  },
-]
+];
