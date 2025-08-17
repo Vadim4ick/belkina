d61c476477a5252c35ffbfb7a711a6f862553171
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-08-17 20:03:04 +0300

    Revert "test: тестирование применения миграций при изменении полей в бд"
    This reverts commit 3fece2f302de912295d94b371720c960f85d68f3.

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -1071,7 +1071,7 @@ export interface HomePage {
   aboutProjectBanner: {
     title: string;
     subtitle: string;
-    description?: {
+    desc?: {
       root: {
         type: string;
         children: {
@@ -1126,7 +1126,7 @@ export interface HomePageSelect<T extends boolean = true> {
     | {
         title?: T;
         subtitle?: T;
-        description?: T;
+        desc?: T;
         media?: T;
       };
   diagnosticTestBanner?:
