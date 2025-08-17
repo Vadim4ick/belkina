0c3d08656ffa5738d6961a7ff5edde62f707dcc7
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-08-17 10:59:47 +0300

    генерация меты вынесена на уровень page роута, иначе не гененироует!

--- src/views/home/ui/Home.tsx
+++ src/views/home/ui/Home.tsx
@@ -7,28 +7,6 @@ import { SliderWrapper } from '@/widgets/slider-wrapper'
 import { getHomePage } from '@/shared/actions/home.action'
 import { getPosts } from '@/shared/actions/post.action'
 import { VerifyEmail } from '@/widgets/verify-email'
-import { Metadata } from 'next'
-
-export async function generateMetadata(): Promise<Metadata> {
-  try {
-    const res = await getHomePage()
-    const meta = res?.HomePage?.Meta || {}
-
-    return {
-      title: meta.seo_title || 'Подготовка к ЕГЭ по русскому языку | BELKINA.ONLINE | Главная',
-      description:
-        meta.seo_description ||
-        'Эффективно готовьтесь к ЕГЭ по русскому с персональной программой на платформе BELKINA.ONLINE',
-    }
-  } catch (error) {
-    console.error('Failed to generate metadata:', error)
-    return {
-      title: 'Подготовка к ЕГЭ по русскому языку | BELKINA.ONLINE | Главная',
-      description:
-        'Эффективно готовьтесь к ЕГЭ по русскому с персональной программой на платформе BELKINA.ONLINE',
-    }
-  }
-}
 
 const Home = async () => {
   const [res, posts] = await Promise.allSettled([getHomePage(), getPosts()])
