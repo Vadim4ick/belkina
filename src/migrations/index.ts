d65d85839387c4492cfea28c966a9cf6c035870a
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-08-17 20:03:04 +0300

    Revert "test: тестирование применения миграций при изменении полей в бд"
    This reverts commit 3fece2f302de912295d94b371720c960f85d68f3.

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,5 @@
 import * as migration_20250817_144404_initial_schema from './20250817_144404_initial_schema';
 import * as migration_20250817_150053_banner_desc_field from './20250817_150053_banner_desc_field';
-import * as migration_20250817_161125_mainOfferBanner_desc_field_renamed_to_description from './20250817_161125_mainOfferBanner_desc_field_renamed_to_description';
 
 export const migrations = [
   {
@@ -11,11 +10,6 @@ export const migrations = [
   {
     up: migration_20250817_150053_banner_desc_field.up,
     down: migration_20250817_150053_banner_desc_field.down,
-    name: '20250817_150053_banner_desc_field',
-  },
-  {
-    up: migration_20250817_161125_mainOfferBanner_desc_field_renamed_to_description.up,
-    down: migration_20250817_161125_mainOfferBanner_desc_field_renamed_to_description.down,
-    name: '20250817_161125_mainOfferBanner_desc_field_renamed_to_description'
+    name: '20250817_150053_banner_desc_field'
   },
 ];
