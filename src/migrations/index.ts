a7244c3a65d8d0ab5befb815f7d934111e5fc3d3
Vadim <a-yva-2013@mail.ru>
2025-08-17 17:57:42 +0400

    new migrate

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250817_124916_initial_schema from './20250817_124916_initial_schema';
 import * as migration_20250817_133838_rename_desc_to_description from './20250817_133838_rename_desc_to_description';
+import * as migration_20250817_135625_sync_schema_snapshot from './20250817_135625_sync_schema_snapshot';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20250817_133838_rename_desc_to_description.up,
     down: migration_20250817_133838_rename_desc_to_description.down,
-    name: '20250817_133838_rename_desc_to_description'
+    name: '20250817_133838_rename_desc_to_description',
+  },
+  {
+    up: migration_20250817_135625_sync_schema_snapshot.up,
+    down: migration_20250817_135625_sync_schema_snapshot.down,
+    name: '20250817_135625_sync_schema_snapshot'
   },
 ];
