703c45090b9306efcf6ac6d34001c63b3574ec6d
Vadim Firulyov <firulvv@mail.ru>
2025-08-17 17:41:00 +0400

    миграция по изменению названия поля

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250817_124916_initial_schema from './20250817_124916_initial_schema';
+import * as migration_20250817_133838_rename_desc_to_description from './20250817_133838_rename_desc_to_description';
 
 export const migrations = [
   {
     up: migration_20250817_124916_initial_schema.up,
     down: migration_20250817_124916_initial_schema.down,
-    name: '20250817_124916_initial_schema'
+    name: '20250817_124916_initial_schema',
+  },
+  {
+    up: migration_20250817_133838_rename_desc_to_description.up,
+    down: migration_20250817_133838_rename_desc_to_description.down,
+    name: '20250817_133838_rename_desc_to_description'
   },
 ];
--- src/views/home/ui/Home.tsx
+++ src/views/home/ui/Home.tsx
@@ -14,8 +14,6 @@ const Home = async () => {
   const resVal = getSettledValue(res)
   const postsVal = getSettledValue(posts)
 
-  console.log(1)
-
   return (
     <>
       {resVal && resVal?.HomePage && resVal?.HomePage?.mainOfferBanner?.title && (
