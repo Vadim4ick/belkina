2c9312cdd00dc0566a57f8bfda0abf480ef9a85e
Vadim <a-yva-2013@mail.ru>
2025-08-21 13:40:07 +0400

    добавление кнопки для перехода к тестам в банере

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250817_201152_init from './20250817_201152_init';
+import * as migration_20250821_093901 from './20250821_093901';
 
 export const migrations = [
   {
     up: migration_20250817_201152_init.up,
     down: migration_20250817_201152_init.down,
-    name: '20250817_201152_init'
+    name: '20250817_201152_init',
+  },
+  {
+    up: migration_20250821_093901.up,
+    down: migration_20250821_093901.down,
+    name: '20250821_093901'
   },
 ];
