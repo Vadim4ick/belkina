1ae8114ab320142023b74683c39fe97972bbabc6
Vadim Firulyov <firulvv@mail.ru>
2025-08-21 23:48:30 +0400

    add seo faq

--- src/app/(frontend)/(public)/faq/page.tsx
+++ src/app/(frontend)/(public)/faq/page.tsx
@@ -1,8 +1,24 @@
 import { Container } from '@/shared/ui/container'
 
 import { FaqPage } from '@/views/faqs'
+import type { Metadata } from 'next/types'
 
 export const revalidate = 180
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: 'Блог Belkina.online',
+    description: 'Часто задаваемые вопросы от Belkina.online.',
+    alternates: {
+      canonical: '/faq',
+    },
+    openGraph: {
+      title: 'Блог Belkina.online',
+      description: 'Часто задаваемые вопросы от Belkina.online.',
+      url: '/faq',
+    },
+  }
+}
 
 async function Page() {
   return (
