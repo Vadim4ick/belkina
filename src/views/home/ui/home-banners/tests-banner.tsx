06f4f81b33d6034084ff801e0c41339177a1e34a
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-08-22 11:02:48 +0300

    стили адаптивности банера на главной

--- src/views/home/ui/home-banners/tests-banner.tsx
+++ src/views/home/ui/home-banners/tests-banner.tsx
@@ -12,11 +12,11 @@ const TestsBanner = ({
 }) => {
   return (
     <section className="bg-light-grey max-mobile:py-6 py-12">
-      <Container className="relative flex max-w-[1200px] items-center justify-center">
+      <Container className="relative flex max-w-[1440px] items-center justify-center">
         <div className="bg-blue relative w-full overflow-hidden rounded-[16px]">
-          <div className="max-tablet:flex-col max-mobile:py-[24px] max-mobile:px-4 max-tablet:h-full flex h-[570px] w-full items-center justify-between gap-6 px-[20px] py-[48px]">
-            <div className="max-tablet:max-w-full z-10 flex h-full w-full max-w-[700px] flex-col justify-between gap-[24px]">
-              <div className="flex flex-col gap-6">
+          <div className="max-tablet:flex-col max-mobile:py-[24px] max-mobile:px-4 max-tablet:h-full flex h-[510px] w-full items-center justify-between gap-6 px-[40px] py-[48px]">
+            <div className="max-tablet:max-w-full max- z-10 flex h-full w-full flex-col justify-center gap-[24px] lg:max-w-[70%]">
+              <div className="flex w-full flex-col gap-6 md:max-w-3/4 lg:max-w-full">
                 <Typography
                   className="max-mobile:text-[32px] text-white"
                   tag="h1"
@@ -46,16 +46,16 @@ const TestsBanner = ({
             <img
               src={'/img/teacher.png'}
               alt="teacher"
-              className="max-mobile:block z-10 -mr-[16px] -mb-[24px] hidden max-h-[350px] self-end"
+              className="max-mobile:block z-10 -mr-[16px] -mb-[24px] hidden h-full self-end"
             />
           </div>
-          <div className="mobile:bottom-0 max-mobile:top-[150px] max-tablet:left-[-20px] absolute w-[1500px]">
+          <div className="mobile:bottom-0 max-mobile:top-[150px] max-tablet:left-[-20px] absolute w-[1500px] opacity-40">
             <img alt="line" src={'/img/bannerLine.png'} />
           </div>
           <img
             src={'/img/teacher.png'}
             alt="teacher"
-            className="max-mobile:hidden absolute right-0 bottom-0 max-w-[400px]"
+            className="max-mobile:hidden absolute right-0 bottom-0 h-full max-w-[400px]"
           />
         </div>
       </Container>
