979d6c33ecf2dbbb3e8047c0cbf90d5bdd1e4f78
Vadim <a-yva-2013@mail.ru>
2025-08-23 18:37:20 +0400

    изименение миграциию

--- src/migrations/20250823_142912_update_tests_description_to_richtext.ts
+++ src/migrations/20250823_142912_update_tests_description_to_richtext.ts
@@ -1,11 +1,19 @@
 import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
 
-export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
+export async function up({ db }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
-   ALTER TABLE "tests" ALTER COLUMN "description" SET DATA TYPE jsonb;`)
+    ALTER TABLE "tests"
+    ALTER COLUMN "description"
+    SET DATA TYPE jsonb
+    USING to_jsonb("description");
+  `)
 }
 
-export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
+export async function down({ db }: MigrateDownArgs): Promise<void> {
   await db.execute(sql`
-   ALTER TABLE "tests" ALTER COLUMN "description" SET DATA TYPE varchar;`)
+    ALTER TABLE "tests"
+    ALTER COLUMN "description"
+    SET DATA TYPE varchar
+    USING "description"::text;
+  `)
 }
