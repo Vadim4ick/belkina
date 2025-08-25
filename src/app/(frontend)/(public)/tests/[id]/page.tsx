a7b02a6b0b9fb8ab115ee3c14dc88a373b442b34
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-08-25 17:57:44 +0300

    генерация мета данных

--- src/app/(frontend)/(public)/tests/[id]/page.tsx
+++ src/app/(frontend)/(public)/tests/[id]/page.tsx
@@ -7,3 +7,12 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
 
   return <TestByIdPage id={id} />
 }
+
+export async function generateMetadata({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params
+
+  return {
+    title: `Тест ${id} | BELKINA.ONLINE | Тесты`,
+    description: `Пройди тест ${id} и подготовься к ЕГЭ по русскому языку с BELKINA.ONLINE`,
+  }
+}
