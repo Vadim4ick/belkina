da11c4fba75373d50e2e79147f7476cf78ec2dbd
Vadim <a-yva-2013@mail.ru>
2025-08-30 13:23:06 +0400

    ✨ (payment-btn.tsx): добавлена навигация для неавторизованных пользователей ✨ (payment-btn.tsx): добавлена навигация для неподтвержденных пользователей Добавлена логика перенаправления для пользователей, которые не авторизованы или не подтвердили свою почту. Это улучшает пользовательский опыт, позволяя пользователям сразу перейти к соответствующим страницам для авторизации или подтверждения почты, что способствует более гладкому процессу оплаты.

--- src/views/webinars-by-slug-page/ui/payment-btn.tsx
+++ src/views/webinars-by-slug-page/ui/payment-btn.tsx
@@ -2,9 +2,11 @@
 
 import { useProfileStore } from '@/entities/user/use-profile-store'
 import { useProfile } from '@/shared/hooks/use-profile'
+import { getRouteAuth, getRouteProfile } from '@/shared/lib/routes'
 import { useCreateWebinarPayment } from '@/shared/services/webinar-payments.service'
 import { useGetWebinarPayment } from '@/shared/services/webinar.service'
 import { Button } from '@/shared/ui/button'
+import { useRouter } from 'next/navigation'
 import { toast } from 'sonner'
 
 const PaymentBtn = ({ webinarId, webinarSlug }: { webinarId: number; webinarSlug: string }) => {
@@ -13,12 +15,16 @@ const PaymentBtn = ({ webinarId, webinarSlug }: { webinarId: number; webinarSlug
 
   const { mutate: createWebinarPayment, isPending: isLoadingPayment } = useCreateWebinarPayment()
 
+  const router = useRouter()
+
   const handleClick = () => {
     if (!!!profile?.id) {
+      router.push(getRouteAuth())
       return toast.error('Вы не авторизованы. Пожалуйста, авторизуйтесь.')
     }
 
     if (!profile.isVerified || !profile.email) {
+      router.push(getRouteProfile())
       return toast.error('Вы не подтвердили почту. Пожалуйста, подтвердите свою почту в профиле.')
     }
 
