8498156a30db6dfd9be254f196b4bf9a36382343
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-09-02 09:49:16 +0300

    обновлены мета страниц согласно SEO

--- src/app/(frontend)/(public)/posts/page/[pageNumber]/page.tsx
+++ src/app/(frontend)/(public)/posts/page/[pageNumber]/page.tsx
@@ -19,15 +19,24 @@ export async function generateMetadata({ params: paramsPromise }: Args): Promise
   const pageNum = parseInt(pageNumber) || 1
 
   return {
-    title: pageNum > 1 ? `Блог Belkina.online - Страница ${pageNum}` : 'Блог Belkina.online',
-    description: 'Последние статьи и новости от Belkina.online. ОГЭ и ЕГЭ по Русскому языку.',
-    keywords: 'Огэ Егэ по русскому языку новости',
+    title:
+      pageNum > 1
+        ? `Блог по подготовке к ОГЭ и ЕГЭ по русскому 2025 - Страница ${pageNum}`
+        : 'Belkina.online',
+    description:
+      'Полезные статьи по подготовке к ОГЭ и ЕГЭ по русскому языку в 2025 году. Разбор заданий, ошибки, советы и обновлённые правила. Узнайте, как сдать экзамен на 90+ без стресса',
+    keywords:
+      'блог по егэ по русскому, подготовка к огэ по русскому 2025, егэ по русскому 2025, разбор заданий егэ, ошибки в егэ по русскому, как сдать егэ на 90, советы по огэ, belkina online, подготовка к егэ без стресса, статьи по русскому языку, новые правила егэ 2025, практика по егэ, онлайн подготовка к огэ, бесплатные материалы егэ, индивидуальные занятия по русскому',
     alternates: {
       canonical: pageNum > 1 ? undefined : '/posts',
     },
     openGraph: {
-      title: pageNum > 1 ? `Блог Belkina.online - Страница ${pageNum}` : 'Блог Belkina.online',
-      description: 'Последние статьи и новости от Belkina.online. ОГЭ и ЕГЭ по Русскому языку.',
+      title:
+        pageNum > 1
+          ? `Блог по подготовке к ОГЭ и ЕГЭ по русскому 2025 - Страница ${pageNum}`
+          : 'Belkina.online',
+      description:
+        'Полезные статьи по подготовке к ОГЭ и ЕГЭ по русскому языку в 2025 году. Разбор заданий, ошибки, советы и обновлённые правила. Узнайте, как сдать экзамен на 90+ без стресса',
       url: pageNum > 1 ? `/posts/page/${pageNum}` : '/posts',
     },
     robots: {
